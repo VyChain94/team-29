b2d5712f6a17f1ebe083b6848620751f74fc1535
Cynthia Guy <cynthiajguy@gmail.com>
2023-09-21 15:21:57 +0000

    Added Character creation and name attr

--- LevelUpGame.Tests/levelup/CharacterTest.cs
+++ LevelUpGame.Tests/levelup/CharacterTest.cs
@@ -1,3 +1,26 @@
+using NUnit.Framework;
+using levelup;
+
 public class CharacterTest {
-    
+    Character chr;
+
+    [SetUp]
+    public void SetUp(){
+        chr = new Character();
+    }
+
+    [Test]
+    public void TestDefaultName() {        
+        string name = chr.getName();
+        string defaultname = "Character";
+        Assert.AreEqual(defaultname,name);
+    }
+
+    [Test]
+    public void TestCustomName() {
+        chr = new Character("Bob");
+        string name = chr.getName();
+        string customName = "Bob";
+        Assert.AreEqual(customName, name);
+    }
 }
--- LevelUpGame/levelup/Character.cs
+++ LevelUpGame/levelup/Character.cs
@@ -1,3 +1,17 @@
 public class Character{
 
+    string name;
+
+    public Character() {
+        this.name = "Character";
+    }
+
+    public Character(string name) {
+        this.name = name;
+    }
+
+    public string getName() {
+        return this.name;
+    }
+
 }
